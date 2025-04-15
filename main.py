fe35c1d8d2f484e451dfaee0c5ef67bccca17312
Peter-Troy <62006529+Peter-Troy@users.noreply.github.com>
2025-04-16 06:19:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import os
 
 # Set up Chrome options for headless operation
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+#chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
