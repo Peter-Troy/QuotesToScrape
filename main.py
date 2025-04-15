7dd14d0adcbe375eead15ccfbc9396d4d470dff4
Peter-Troy <62006529+Peter-Troy@users.noreply.github.com>
2025-04-16 06:24:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ chrome_options = Options()
 #chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--user-data-dir=/tmp/chrome_data")
 
 # ChromeDriver setup
 chromedriver_path = '/usr/local/bin/chromedriver'
